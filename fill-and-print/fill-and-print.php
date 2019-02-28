81ae32817265650d08676d382b1c00da1ee6d136
Rui Miguel <ravage@fragmentized.net>
2019-02-28 15:57:10 +0000

    Initial commit

---
+++ fill-and-print/fill-and-print.php
@@ -0,0 +1,16 @@
+<?php
+
+function fill(int $size = 10) : array
+{
+    $values = array();
+    for ($i = 0; $i < $size; $i++) {
+        $values[] = rand(1, 100);
+    }
+
+    return $values;
+}
+
+$values = fill();
+array_walk($values, function($element) {
+    echo "$element\n";
+});
