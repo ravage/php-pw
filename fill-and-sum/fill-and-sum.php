d0e0a96783898a8fbf93c6329f462000603920ed
Rui Miguel <ravage@fragmentized.net>
2019-03-01 00:43:40 +0000

    Add fill-and-sum

---
+++ fill-and-sum/fill-and-sum.php
@@ -0,0 +1,14 @@
+<?php
+
+function fill(int $capacity = 10) : array
+{
+    $values = array();
+
+    for ($i = 0; $i < $capacity; $i++) {
+        $values[] = rand(1, 100);
+    }
+
+    return $values;
+}
+
+echo array_sum(fill()) . "\n";
